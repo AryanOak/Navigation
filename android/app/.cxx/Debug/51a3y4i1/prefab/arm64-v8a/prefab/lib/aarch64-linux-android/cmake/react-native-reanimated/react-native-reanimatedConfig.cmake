82165de1a079c04ce949ee3c1f41893635a0d476
AryanOak2 <aryanoak16@gmail.com>
2026-02-18 15:18:11 +0530

    Changed By Aryan Oak on 18 Feb 26,  Created a React native Cli Project

---
+++ android/app/.cxx/Debug/51a3y4i1/prefab/arm64-v8a/prefab/lib/aarch64-linux-android/cmake/react-native-reanimated/react-native-reanimatedConfig.cmake
@@ -0,0 +1,9 @@
+if(NOT TARGET react-native-reanimated::reanimated)
+add_library(react-native-reanimated::reanimated SHARED IMPORTED)
+set_target_properties(react-native-reanimated::reanimated PROPERTIES
+    IMPORTED_LOCATION "D:/Vs Code Projects/Navigation/node_modules/react-native-reanimated/android/build/intermediates/cxx/Debug/c5m5s3u1/obj/arm64-v8a/libreanimated.so"
+    INTERFACE_INCLUDE_DIRECTORIES "D:/Vs Code Projects/Navigation/node_modules/react-native-reanimated/android/build/prefab-headers/reanimated"
+    INTERFACE_LINK_LIBRARIES ""
+)
+endif()
+
